0c867702be6deaf513b7e4c4c87c6ff2c8df6008
DEI ROMERO <DEI ROMERO@DESKTOP-UNSED2L>
2022-05-15 17:33:03 -0500

    Finish Class City

--- Shopping/Shopping/Data/Entities/City.cs
+++ Shopping/Shopping/Data/Entities/City.cs
@@ -6,7 +6,7 @@ namespace Shopping.Data.Entities
     {
 
         public int Id { get; set; }
-        //DataAnnotations//
+        
         [Display(Name = "Ciudad")]
         [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
         [Required(ErrorMessage = "El Campo {0} es obligatorio.")]
